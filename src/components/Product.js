cc5d86e3ea217ff63c8822a040947cfb990a6a87
Vinh Hong <hongthevinh@gmail.com>
2023-03-13 19:36:59 -0700

    can change status of image to make dark border

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import { data } from '../data';
 import '../css/product.css'
 import classnames from 'classnames';
@@ -6,14 +6,29 @@ import classnames from 'classnames';
 
 
 export default function Product() {
+  const [cardList, setCardList] = useState(data);
 
+  const handleCardClick = (card, index) => {
+    //clone current array to the new one
+    const newCardList = [...cardList]
+
+    console.log(card, index);
+    // toogle state
+    const newCard = {
+      ...newCardList[index],
+      status: newCardList[index].status === 'omitted' ? 'selected' : 'omitted',
+    };
+    newCardList[index] = newCard;
+
+    setCardList(newCardList);
+  }
 
   return (
     <>
       <section className="product">
         <div className="container" >
           <div className="row" >
-            {data.map((card, index) => {
+            {cardList.map((card, index) => {
               return (
                 <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 product-item" key={index} >
                   <div className="card_image" >
@@ -23,7 +38,9 @@ export default function Product() {
                         "card-img-top": true,
                         selected: card.status === 'selected'
                       })}
-                      alt={card.cardName} />
+                      alt={card.cardName}
+                      onClick={() => handleCardClick(card, index)}
+                    />
                   </div>
                 </div>
               )
